b75597f8e4ab1267503879b3a56e649062d8dd31
DetGrey <anenovlar@gmail.com>
2024-02-22 11:26:03 +0100

    binary search implemented

--- app/src/main/java/com/example/linear_search/binary-search.kt
+++ app/src/main/java/com/example/linear_search/binary-search.kt
@@ -3,8 +3,9 @@ package com.example.linear_search
 fun main() {
     val list = (1..50).toList()
     val list2 = listOf(1,2,4,5,6,7,8,9,10)
-    println(binarySearch(list, 26, 0, list.size))
+//    println(binarySearch(list, 26, 0, list.size))
 //    println(binarySearch(list2, 3, 0, list2.size))
+    println(binarySearch2(list, 33))
 }
 
 /*
@@ -34,4 +35,28 @@ fun binarySearch(array: List<Int>, target: Int, min: Int, max: Int): Int {
     else {
         binarySearch(array, target, min, mid - 1)
     }
+}
+
+fun binarySearch2(array: List<Int>, target: Int): Int {
+    var min = 0
+    var max = array.size
+
+    if (min == max ) {
+        return -1
+    }
+
+    while (min <= max) {
+        val mid = (min + max) / 2
+
+        println("Min: $min, max: $max, mid: $mid")
+
+        println("mid value: ${array[mid]}")
+
+        when {
+            array[mid] == target -> return mid
+            array[mid] > target -> max = mid - 1
+            array[mid] < target -> min = mid + 1
+        }
+    }
+    return -1
 }
